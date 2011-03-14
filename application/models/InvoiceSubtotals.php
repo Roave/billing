2cb21bd8ede336239575db6eb5466941b6a3e748
Ben Youngblood <bx.youngblood@gmail.com>
2011-03-14 12:28:13 -0700

    override insert/createRow/delete/update, as those should not be performed on a view

--- application/models/InvoiceSubtotals.php
+++ application/models/InvoiceSubtotals.php
@@ -19,4 +19,38 @@ class InvoiceSubtotals extends Zend_Db_Table_Abstract {
 			'columns' => 'invoice_id', 
 			'refTableClass' => 'Invoices', 
 			'refColumns' => 'invoice_id'));
+	
+	/* (non-PHPdoc)
+	 * @see Zend_Db_Table_Abstract::insert()
+	 */
+	public function insert(array $data) {
+		throw new InvalidMethodException('Cannot insert into a view');
+	}
+	
+	/* (non-PHPdoc)
+	 * @see Zend_Db_Table_Abstract::createRow()
+	 */
+	public function createRow(array $data = array(), $defaultSource = null)
+	{
+		throw new InvalidMethodException('Cannot insert into a view');
+	}
+
+	/* (non-PHPdoc)
+	 * @see Zend_Db_Table_Abstract::delete()
+	 */
+	public function delete($where)
+	{
+		throw new InvalidMethodException('Cannot delete from a view');
+	}
+
+	/* (non-PHPdoc)
+	 * @see Zend_Db_Table_Abstract::update()
+	 */
+	public function update(array $data, $where)
+	{
+		throw new InvalidMethodException('Cannot update a view');
+	}
+
+	
+	
 }
