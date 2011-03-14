42b0f16805d7c14f30c95755a260b4ca629792c9
Ben Youngblood <bx.youngblood@gmail.com>
2011-03-14 16:46:52 -0700

    add line items to invoices

--- application/controllers/InvoicesController.php
+++ application/controllers/InvoicesController.php
@@ -6,10 +6,12 @@
  * @author Ben Youngblood <bx.youngblood@gmail.com>
  */
 
-require_once "../application/models/Invoices.php";
-require_once "../application/models/Customers.php";
-require_once "../application/models/InvoiceDetails.php";
-require_once "../application/models/InvoiceSubtotals.php";
+require_once APPLICATION_PATH . '/models/Invoices.php';
+require_once APPLICATION_PATH . '/models/Products.php';
+require_once APPLICATION_PATH . '/models/Customers.php';
+require_once APPLICATION_PATH . '/models/InvoiceDetails.php';
+require_once APPLICATION_PATH . '/models/InvoiceSubtotals.php';
+require_once APPLICATION_PATH . '/models/CustomerProducts.php';
 
 class InvoicesController extends Zend_Controller_Action {
 
@@ -47,11 +49,148 @@ class InvoicesController extends Zend_Controller_Action {
 		$this->view->details = $invoice->findDependentRowset('InvoiceDetails');
 		$this->view->subtotal = $invoice->findDependentRowset('InvoiceSubtotals')
 			->current();
+		
+		$customerProductsTable = new CustomerProducts();
+		$products = $customerProductsTable->findProductsCoalesced($invoice->customer_id);
+		
+		$productNames = array(
+			0 => '');
+		$productData = array();
+		if ($products) {
+			foreach ($products as $p) {
+				$productNames[] = $p['name'];
+				$productData[$p['product_id']] = $p;
+			}
+		}
+		
+		$this->view->products = $productNames;
+		$this->view->productData = $productData;
 	}
 
 	public function newAction()
 	{
+		$form = $this->getNewInvoiceForm();
+		echo $form;
+	}
+
+	public function saveAction()
+	{
 		$invoicesTable = new Invoices();
+		$form = $this->getNewInvoiceForm();
 		
+		if ($form->isValid($_POST)) {
+			$values = $form->getValues();
+			
+			$data = array(
+				'customer_id' => $values['customer'], 
+				'invoice_date' => $values['invoice_date'], 
+				'due_date' => $values['due_date'], 
+				'notes' => $values['notes']);
+			
+			$id = $invoicesTable->insert($data);
+			
+			$this->_redirect('/invoices/view/id/' . $id);
+		} else {
+			echo $form;
+		}
+	}
+
+	public function updateAction()
+	{
+		switch ($this->_getParam('do')) {
+			case 'add-line-item' :
+				$this->addLineItem();
+				break;
+			default :
+				throw new Exception();
+				break;
+		}
+	}
+
+	private function addLineItem()
+	{
+		$invoiceId = $this->_getParam('id');
+		$sku = $this->_getParam('sku');
+		$productId = $this->_getParam('product');
+		$unitPrice = $this->_getParam('unit_price');
+		$quantity = $this->_getParam('quantity');
+		
+		$invoicesTable = new Invoices();
+		$invoice = $invoicesTable->find($invoiceId)
+			->current();
+		
+		$customerProductsTable = new CustomerProducts();
+		$product = $customerProductsTable->findProductCoalesced($invoice->customer_id, $productId);
+		
+		$data = array(
+			'invoice_id' => $invoice->invoice_id, 
+			'sku' => $sku, 
+			'name' => $product['name'], 
+			'unit_price' => $unitPrice, 
+			'quantity' => $quantity);
+		
+		$detailsTable = new InvoiceDetails();
+		$detailsTable->insert($data);
+		
+		unset($data['invoice_id']);
+		unset($data['quantity']);
+		unset($data['name']);
+		$data['product_id'] = $productId;
+		$data['customer_id'] = $invoice->customer_id;
+		
+		$customerProductsTable->updateCustomerProduct($data);
+		
+		$this->_redirect('/invoices/view/id/' . $invoiceId);
+	}
+
+	private function getNewInvoiceForm(array $values = null)
+	{
+		$customersTable = new Customers();
+		$customers = $customersTable->fetchAll();
+		
+		$customerList = array();
+		if ($customers->count() > 0) {
+			foreach ($customers as $c) {
+				$customerList[$c->customer_id] = $c->company;
+			}
+		}
+		
+		$form = new Zend_Form();
+		$form->setAction('/invoices/save')
+			->setMethod('post');
+		
+		$customer = new Zend_Form_Element_Select('customer');
+		$customer->addMultiOptions($customerList)
+			->setLabel('Customer');
+		
+		$invoiceDate = new Zend_Form_Element_Text('invoice_date');
+		$invoiceDate->setRequired(true)
+			->setLabel('Invoice Date')
+			->addValidator(new Zend_Validate_Date());
+		
+		$dueDate = new Zend_Form_Element_Text('due_date');
+		$dueDate->setRequired(true)
+			->setLabel('Due Date')
+			->addValidator(new Zend_Validate_Date());
+		
+		$notes = new Zend_Form_Element_Textarea('notes');
+		$notes->setLabel('Notes');
+		
+		$submit = new Zend_Form_Element_Submit('submit');
+		
+		if (isset($values)) {
+			$customer->setValue($values['customer_id']);
+			$invoiceDate->setValue($values['invoice_date']);
+			$dueDate->setValue($values['due_date']);
+			$notes->setValue($values['notes']);
+		}
+		
+		$form->addElement($customer)
+			->addElement($invoiceDate)
+			->addElement($dueDate)
+			->addElement($notes)
+			->addElement($submit);
+		
+		return $form;
 	}
 }
--- application/models/CustomerProducts.php
+++ application/models/CustomerProducts.php
@@ -8,6 +8,8 @@
  * @author Ben Youngblood <bx.youngblood@gmail.com> 
  */
 
+require_once "../application/models/Products.php";
+
 class CustomerProducts extends Zend_Db_Table_Abstract {
 
 	/**
@@ -15,29 +17,111 @@ class CustomerProducts extends Zend_Db_Table_Abstract {
 	 * @var string
 	 */
 	protected $_name = 'customer_products';
-	
+
 	/**
 	 * Primary key
 	 * @var array
 	 */
-	protected $_primary = array('product_id', 'customer_id');
-	
+	protected $_primary = array(
+		'product_id', 
+		'customer_id');
+
 	/**
 	 * The PK is not a sequence
 	 */
 	protected $_sequence = false;
-	
+
 	protected $_referenceMap = array(
 		'Customer' => array(
-			'columns'		=>	'customer_id',
-			'refTableClass'	=>	'Customers',
-			'refColumns'	=>	'customer_id'
-		),
+			'columns' => 'customer_id', 
+			'refTableClass' => 'Customers', 
+			'refColumns' => 'customer_id'), 
 		'Product' => array(
-			'columns'		=>	'product_id',
-			'refTableClass'	=>	'Products',
-			'refColumns'	=>	'product_id'
-		)
-	);
+			'columns' => 'product_id', 
+			'refTableClass' => 'Products', 
+			'refColumns' => 'product_id'));
+
+	/**
+	 * @param int $customerId
+	 */
+	public function findProductsCoalesced($customerId)
+	{
+		$productsTable = new Products();
+		$products = $productsTable->fetchAll();
+		
+		$productsCoalesced = array();
+		if ($products->count() > 0) {
+			foreach ($products as $p) {
+				$cp = $this->find($p->product_id, $customerId)
+					->current();
+				
+				if ($cp) {
+					$productsCoalesced[] = array(
+						'product_id' => $cp->product_id, 
+						'sku' => $cp->sku ?  : $p->sku, 
+						'name' => $cp->name ?  : $p->name, 
+						'description' => $cp->description ?  : $p->description, 
+						'unit_price' => $cp->unit_price ?  : $p->unit_price);
+				} else {
+					$productsCoalesced[] = array(
+						'product_id' => $p->product_id, 
+						'sku' => $p->sku, 
+						'name' => $p->name, 
+						'description' => $p->description, 
+						'unit_price' => $p->unit_price);
+				}
+			}
+		}
+		
+		return $productsCoalesced;
+	}
+
+	/**
+	 * @param int $customerId
+	 * @param int $productId
+	 */
+	public function findProductCoalesced($customerId, $productId)
+	{
+		$productsTable = new Products();
+		$p = $productsTable->find($productId)
+			->current();
+		
+		$cp = $this->find($productId, $customerId)
+			->current();
+		
+
+		if ($cp) {
+			$productCoalesced= array(
+				'product_id' => $cp->product_id, 
+				'sku' => $cp->sku ?  : $p->sku, 
+				'name' => $cp->name ?  : $p->name, 
+				'description' => $cp->description ?  : $p->description, 
+				'unit_price' => $cp->unit_price ?  : $p->unit_price);
+		} else {
+			$productCoalesced = array(
+				'product_id' => $p->product_id, 
+				'sku' => $p->sku, 
+				'name' => $p->name, 
+				'description' => $p->description, 
+				'unit_price' => $p->unit_price);
+		}
+		
+		return $productCoalesced;
+	}
+
+	public function updateCustomerProduct(array $data)
+	{
+		$record = $this->find($data['product_id'], $data['customer_id'])->current();
+		if ($record) {
+			$record->sku = $data['sku'];
+			$record->unit_price = $data['unit_price'];
+			$record->save();
+			
+			return;
+		} else {
+			$this->insert($data);
+			return;
+		}
+	}
 
 }
